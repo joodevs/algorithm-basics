df63eee8f3c2407650258fbe33e369c405240f00
Jooyoung Oh <jooyoungoh@Jooyoungs-MBP.local>
2023-10-26 16:50:35 +0900

    Added queue

--- search.py
+++ search.py
@@ -33,3 +33,29 @@ print(stack[::-1])
 
 # [5, 2, 3, 1]
 # [1, 3, 2, 5]
+
+# 큐
+# 큐는 선입선출 (First In First Out) 인 대기 줄에 비유할 수 있으며, 고로 공정하다 표현한다.
+
+from collections import deque
+
+# 큐(Queue) 구현을 위해 deque 라이브러리 사용
+queue = deque()
+
+# 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue)
+queue.reverse()
+print(queue)
+
+# 파이썬으로 큐를 구현할 때는 collections 모듈에서 제공하는 deque 자료구조를 활용한다.
+# deque 은 스택과 큐의 장점을 모두 채택한 것인데 데이터를 넣고 빼는 속도가 리스트 자료형에 비해 효율적이며 queue 라이브러리 보다 간단하다.
+# deque 객체를 리스트 자료형으로 변경하고자 한다면 list() method 를 이용하자 - list(queue) 를 하면 리스트가 반환된다.
