9efcd96e2bc31fb98e3c91df377a5619a252b7df
Jooyoung Oh <JooyoungOh2026@u.northwestern.edu>
2024-05-07 10:30:09 +0900

    Add Example 1

--- binary-search.py
+++ binary-search.py
@@ -107,3 +107,30 @@ else:
 import sys
 input_data = sys.stdin.readline().rstrip()
 print(input_data)
+
+
+# 실전 문제 - 부품 찾기
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+n = int(input())
+array = list(map(int, input().split()))
+array.sort()
+
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    result = binary_search(array, i, 0, n - 1)
+    if result != None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
